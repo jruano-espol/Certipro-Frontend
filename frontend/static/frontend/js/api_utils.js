507efc0fb226604371d20d003e1498c04f7aac82
Peza <152343973+ManzanillaSebastian@users.noreply.github.com>
2026-06-22 05:32:28 +0000

    Added: global variable API_UTILS module for useful functions

---
+++ frontend/static/frontend/js/api_utils.js
@@ -0,0 +1,37 @@
+
+const API_UTILS={
+    baseUrl: window.API_BASE_URL,
+    
+    get_headers(){
+        const accessToken = localStorage.getItem('access_token');
+        return { 'Authorization': `Bearer ${accessToken}` }
+    },
+
+    async get(endpoint) {
+        try {
+            const response = await fetch(`${this.baseUrl}${endpoint}`, {
+                method: 'GET',
+                headers: this.get_headers()
+            });
+
+            // Si hay error de autenticación (Token expirado), redirigir al login
+            if (response.status === 401) {
+                localStorage.clear();
+                window.location.href = "/select-profile/"; // Ajusta a tu URL de Django
+                return null;
+            }
+
+            if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+            return await response.json();
+        } catch (error) {
+            console.error(`Error en GET ${endpoint}:`, error);
+            throw error;
+        }
+    },
+    async get_by_id(endpoint, id){
+        return await this.get(`${endpoint}${id}/`)
+    },
+    async get_filter_by_column(endpoint, column, id){
+        return await this.get(`${endpoint}?${column}=${id}`)
+    }
+};
